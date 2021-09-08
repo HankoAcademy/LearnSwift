da9d903bd2edddee8f3ac92a1c727ce3022a59b4
Han Kim <hanniek00@gmail.com>
2021-09-08 15:20:07 -0500

    Create a UIView subclass used by ViewController to show how UIScrollViews work

--- UIKit/UIScrollView/UIScrollView/Controller/ViewController.swift
+++ UIKit/UIScrollView/UIScrollView/Controller/ViewController.swift
@@ -8,12 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    override func loadView() {
+        // Go to ContentView class to see how the views are set up for a scroll view
+        view = ContentView()
     }
-
-
 }
 
