4ef8443c56130641ae385cbe5b32545811e1fb21
Han Kim <hanniek00@gmail.com>
2021-09-10 19:17:56 -0500

    Create base Xcode project for UITableViews

---
+++ UIKit/UITableView/UITableView/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableView
+//
+//  Created by Hannie Kim on 9/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
