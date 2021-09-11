29af3c2dd4fd2ec27ef0428d23d5d16626097901
Han Kim <hanniek00@gmail.com>
2021-09-10 21:34:55 -0500

    Add a table view with temporary data

--- UIKit/UITableView/UITableView/Controller/ViewController.swift
+++ UIKit/UITableView/UITableView/Controller/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    var contentView: ContentView!
+    var tableView: UITableView!
+    
+    override func loadView() {
+        contentView = ContentView()
+        view = contentView
+        
+        tableView = contentView.tableView
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Cell tapped")
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        "HI"
+//    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return UITableViewHeaderFooterView()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
+}
+
+class CustomTableViewHeader: UITableViewHeaderFooterView {
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+}
---
+++ UIKit/UITableView/UITableView/View/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  UITableView
+//
+//  Created by Hannie Kim on 9/10/21.
+//
+
+import UIKit
+
+class ContentView: UIView {
+
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUpViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpViews() {
+        
+        addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+}
